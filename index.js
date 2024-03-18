803d48421e09a5f6814cb94af11ce0bb0091a152
+ <+>
2024-03-18 15:31:08 +0300

    used destructive and non-destructive methods to maipulate arrays

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+// Write your solution here
+const cats =["Milo","Otis","Garfield"];
+
+function appendCat(name){
+   const newArray = cats.slice();
+    newArray.push(name);
+    return newArray
+}
+function prependCat(name){
+    const newArray = cats.slice();
+    newArray.unshift(name)
+    return newArray
+}
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function appendCat(name){
+    const newArray = cats.slice();
+    newArray.push(name)
+    return newArray
+}
+function removeFirstCat(name){
+    const newArray = cats.slice();
+    newArray.shift()
+    return newArray
+}
+function removeLastCat(name){
+    const newArray = cats.slice();
+    newArray.pop()
+    return newArray
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift();
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop();
+}
